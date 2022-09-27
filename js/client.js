bfb027282a2850c9ebbb81088f907e6c2b3fd45c
yash <yashwable12@gmail.com>
2022-09-27 23:27:55 +0530

    done with socket.io

--- js/client.js
+++ js/client.js
@@ -3,6 +3,7 @@ const socket = io('http://localhost:8000');
 const form = document.getElementById('send-container');
 const messageInp = document.getElementById('messageInp');
 const messageContainer = document.querySelector(".container");
+const audio = new Audio('ting.mp3');
 
 const append = (message , position) => {
     const messageElement = document.createElement('div');
@@ -10,12 +11,17 @@ const append = (message , position) => {
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
+    if (position === 'left'){
+        audio.play();
+    }
 }
 
 form.addEventListener('submit' ,(e) => {
     e.preventDefault();
     const message = messageInp.value;
     append(`You: ${message}`,'right');
+    socket.emit('send',message);
+    messageInp.value = '';
 })
 
 let names = prompt("Enter your name to join");
@@ -23,4 +29,12 @@ socket.emit('new-user-joined', names);
 
 socket.on('user-joined', names => {
     append(`${names} joined the chat`,'right');
+})
+
+socket.on ('receive',(data) => {
+    append(`${data.names} : ${data.message}`,'left');
+})
+
+socket.on('left',names => {
+    append(`${names} left the chat`,'left');
 })
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -9,12 +9,17 @@ const users = {};
 
 io.on ('connection',socket => {
     socket.on('new-user-joined',names => {
-        console.log("new user",names);
+        // console.log("new user",names);
         users[socket.id] = names;
         socket.broadcast.emit('user-joined',names); 
     });
 
     socket.on('send',message => {
         socket.broadcast.emit('receive',{message:message, names: users[socket.id]});
+    });
+
+    socket.on('disconnect', message => {
+        socket.broadcast.emit('left',users[socket.id]);
+        delete users[socket.id];
     });
 })
